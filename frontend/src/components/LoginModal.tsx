cc556e11c44c43b7e0c79eae6ded45d312683533
adramelech-123 <tmamvuto6@gmail.com>
2024-06-02 13:11:17 +0200

    Form design final

--- frontend/src/components/LoginModal.tsx
+++ frontend/src/components/LoginModal.tsx
@@ -1,10 +1,15 @@
-import { Button, Form, Modal } from "react-bootstrap";
+import { useState } from "react";
+import { Button, FloatingLabel, Form, InputGroup, Modal } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import MainStyles from "../styles/Main.module.css";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 interface LoginModalProps {
   onDismiss: () => void;
 }
 
 const LoginModal = ({onDismiss}: LoginModalProps) => {
+  const [showPassword, setShowPassword] = useState(false); 
   return (
     <Modal show onHide={onDismiss}>
       <Modal.Header closeButton>
@@ -13,16 +18,43 @@ const LoginModal = ({onDismiss}: LoginModalProps) => {
 
       <Modal.Body>
         <Form>
+          <p className="text-muted fs-5">Enter your master account details.</p>
           <Form.Group className="mb-3">
-            <Form.Label>Username</Form.Label>
-            <Form.Control type="text" placeholder="Username" />
+            <FloatingLabel label="Username" className="text-muted">
+              <Form.Control type="text" placeholder="Username" />
+            </FloatingLabel>
           </Form.Group>
           <Form.Group className="mb-3">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <FloatingLabel label>
+              <InputGroup>
+                <Form.Control
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  className="py-3"
+                />
+                <Button
+                  variant="secondary"
+                  className={`text-secondary bg-white ${MainStyles.eyeBtn}`}
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <FaEyeSlash className="text-secondary" />
+                  ) : (
+                    <FaEye className="text-secondary" />
+                  )}
+                </Button>
+              </InputGroup>
+            </FloatingLabel>
           </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
+          <Form.Group
+            className="mb-5 d-flex justify-content-between align-items-center"
+            controlId="formBasicCheckbox"
+          >
+            <Form.Check type="checkbox" label="Remember me" />
+            <Link to={"/"}>Forgot Password?</Link>
+          </Form.Group>
+          <Button variant="primary" size="lg" type="submit">
+            Login Now
           </Button>
         </Form>
       </Modal.Body>
--- frontend/src/components/SignUpModal.tsx
+++ frontend/src/components/SignUpModal.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
-import { Button, Form, InputGroup, Modal } from "react-bootstrap"
+import { Button, FloatingLabel, Form, InputGroup, Modal } from "react-bootstrap"
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+import MainStyles from "../styles/Main.module.css"
 
 interface SignUpModalProps {
   onDismiss: () => void
@@ -20,33 +22,59 @@ const SignUpModal = ({onDismiss}: SignUpModalProps) => {
 
       <Modal.Body>
         <Form>
+          <p className="text-muted fs-5">Create your Master Account.</p>
           <Form.Group className="mb-3">
-            <Form.Label className="fw-bold">Username</Form.Label>
-            <Form.Control type="text" placeholder="Choose a Username" />
+            <FloatingLabel label="Username" className="text-muted">
+              <Form.Control type="text" placeholder="Choose a Username" />
+            </FloatingLabel>
           </Form.Group>
           <Form.Group className="mb-3">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Email" />
+            <FloatingLabel label="Email" className="text-muted">
+              <Form.Control type="email" placeholder="Email" />
+            </FloatingLabel>
           </Form.Group>
           <Form.Group className="mb-3">
-            <Form.Label>Password</Form.Label>
-            <InputGroup>
-              <Form.Control type={showPassword ? 'text' : 'password'} placeholder="Password" />
-              <Button className="bg-white border-secondary " onClick={() => setShowPassword(!showPassword)}>
-                {showPassword ? <FaEyeSlash className="text-secondary"/> : <FaEye className="text-secondary"/>}
-              </Button>
-            </InputGroup>
+            <FloatingLabel label>
+              <InputGroup>
+                <Form.Control
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  className="py-3"
+                />
+                <Button
+                  variant="secondary"
+                  className={`text-secondary bg-white ${MainStyles.eyeBtn}`}
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <FaEyeSlash className="text-secondary" />
+                  ) : (
+                    <FaEye className="text-secondary" />
+                  )}
+                </Button>
+              </InputGroup>
+            </FloatingLabel>
           </Form.Group>
           <Form.Group className="mb-5" controlId="formBasicCheckbox">
             <Form.Check
               type="checkbox"
               checked={termsChecked}
-              label="I agree to the Terms & Conditions"
+              label={
+                <>
+                  By signing up to create an account I accept the company's{" "}
+                  <Link to={"/"}> Terms of Use & Privacy Policy</Link>
+                </>
+              }
               onChange={() => setTermsChecked(!termsChecked)}
             />
           </Form.Group>
-          <Button variant="primary" type="submit" disabled={!termsChecked}>
-            Submit
+          <Button
+            variant="primary"
+            type="submit"
+            size="lg"
+            disabled={!termsChecked}
+          >
+            Sign Up Now
           </Button>
         </Form>
       </Modal.Body>
